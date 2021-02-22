81842400723fc5d799553c1f8011004cd90d84e9
Aditya Upneja <adityaupneja2409@gmail.com>
2021-02-22 18:41:48 +0530

    updated routes

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -1,6 +1,6 @@
 import os
-# import secrets
-# import OpenSSL
+import secrets
+import OpenSSL
 from PIL import Image
 from flask import render_template, url_for, flash, redirect, request, abort
 from flaskblog import app, db, bcrypt
@@ -59,19 +59,19 @@ def logout():
     logout_user()
     return redirect(url_for('home'))
 
-# def save_picture(form_picture):
-#     random_hex = secrets.token_hex(8)
-#     _,f_ext = os.path.splitext(form_picture.filename)
-#     picture_fn = random_hex + f_ext
-#     picture_path = os.path.join(app.root_path,'static/profile_pics', picture_fn)
+ def save_picture(form_picture):
+     random_hex = secrets.token_hex(8)
+     _,f_ext = os.path.splitext(form_picture.filename)
+     picture_fn = random_hex + f_ext
+     picture_path = os.path.join(app.root_path,'static/profile_pics', picture_fn)
     
-#     output_size = (125, 125)
-#     i = Image.open(form_picture)
-#     i.thumbnail(output_size)
+     output_size = (125, 125)
+     i = Image.open(form_picture)
+     i.thumbnail(output_size)
     
-#     i.save(picture_path)
+     i.save(picture_path)
 
-#     return picture_fn
+     return picture_fn
 
 @app.route("/account", methods=['GET', 'POST'])
 @login_required
